5a4ea49faa491700730d1138918c494540dcabd8
Psychee <15145032896@163.com>
2017-06-20 21:07:57 +0800

    音乐播放器

---
+++ vue-project/src/components/movie/Movie.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="movie">
+    <common-header></common-header>
+    <movie-nav></movie-nav>
+    <common-footer></common-footer>
+  </div>
+</template>
+
+<script>
+
+  import CommonHeader from '../common/CommonHeader'
+  import CommonFooter from '../common/CommonFooter'
+  import MovieNav from './MovieNav'
+
+export default {
+  data() {
+    return {
+    }
+  },
+  components:{
+    CommonHeader,
+    MovieNav,
+    CommonFooter
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+  @import "../../assets/css/reset.css";
+
+
+</style>
